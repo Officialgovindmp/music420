1a29acf32fbc4a805756559d087c6eb77eeb1adc
Officialgovindmp <125728619+Officialgovindmp@users.noreply.github.com>
2023-05-19 18:12:03 +0530

    thumbnails.py

--- AlexaMusic/utils/thumbnails.py
+++ AlexaMusic/utils/thumbnails.py
@@ -1,7 +1,14 @@
+ 
+#
+# Copyright (C) 2021-2022 by Alexa_Help @ Github, < https://github.com/Jankarikiduniya >.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# All rights reserved. Â© Alisha Â© Alexa Â© Yukki Â© AnonXMusic
+
+
 import os
 import re
 import textwrap
-import random
+
 import aiofiles
 import aiohttp
 import numpy as np
@@ -9,47 +16,9 @@ import numpy as np
 from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from youtubesearchpython.__future__ import VideosSearch
 
-import config
-from alexa import app
-YOUTUBE_IMG_URL = [ 
+from config import YOUTUBE_IMG_URL
+from AlexaMusic import app
 
-    "https://te.legra.ph/file/9bd2e4063d9827b6ccf77.jpg",
-    "https://te.legra.ph/file/693469f77e9093040b9dc.jpg",
-    "https://te.legra.ph/file/a667c504b964b5745e37b.jpg",
-    "https://te.legra.ph/file/7601b1a7949e2a1468937.jpg",
-    "https://te.legra.ph/file/449457d248ed9f3086658.jpg",
-    "https://te.legra.ph/file/6d1b59a2196ddba142d97.jpg",
-    "https://graph.org/file/f55bd5ce7beb58caf9d6f.jpg",
-    "https://graph.org/file/fad73ad98144dfde187ee.jpg",
-    "https://graph.org/file/315ccf290c21d6bfc3b33.jpg",
-    "https://graph.org/file/41e25722260f4d085e1e3.jpg",
-    "https://graph.org/file/f89b8dff932cbf806cf6f.jpg",
-    "https://graph.org/file/52d424a6a0db206dd0cc4.jpg",
-    "https://graph.org/file/eeb571489e0eafa9d6945.jpg",
-    "https://graph.org/file/5c243867343e2ed9042e7.jpg",
-    "https://graph.org/file/3c2dc17cdb322145afd7e.jpg",
-    "https://graph.org/file/89c0108ea5f14fe95c6e4.jpg",
-    "https://graph.org/file/4be16e32f3079c1dc7504.jpg",
-    "https://graph.org/file/ef886c69173fcd67493eb.jpg",
-    "https://graph.org/file/8c80341d8c76ab5a1f760.jpg",
-    "https://graph.org/file/b124047af598024dc699f.jpg",
-    "https://graph.org/file/f19b7fd5e246bfa65ba81.jpg",
-    "https://graph.org/file/ed5a2d5f715347eefcf89.jpg",
-    "https://graph.org/file/2d7275e5943e4929c72ac.jpg",
-    "https://graph.org/file/1c14887b069dc17f97996.jpg",
-    "https://graph.org/file/493391d43f7f458f227bb.jpg",
-    "https://graph.org/file/826023a1e4b2cbd203d4e.jpg",
-    "https://graph.org/file/cb75c21196aba302cf4d7.jpg",
-    "https://graph.org/file/78355497f093cd68b0716.jpg",
-    "https://graph.org/file/a5f651a5ba622a289b205.jpg",
-    "https://graph.org/file/f710ad6dcdeae5fa54f36.jpg",
-    "https://graph.org/file/3331d74a285a8f107dc7c.jpg",
-    "https://graph.org/file/532d2aa76da6bd56151cc.jpg",
-    "https://graph.org/file/bd0f8a134e4019e35d3b6.jpg",
-    "https://graph.org/file/371e01e799d6460c6d962.jpg",
-    "https://graph.org/file/811fe1a035cfd0adc572b.jpg",
-    "https://graph.org/file/00e84f696f509e9904890.jpg",
-    ]
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
@@ -69,7 +38,7 @@ def add_corners(im):
     im.putalpha(mask)
 
 
-async def gen_thumb(videoid, user_id):
+async def gen_thumb(videoid, user_id, theme):
     if os.path.isfile(f"cache/{videoid}_{user_id}.png"):
         return f"cache/{videoid}_{user_id}.png"
     url = f"https://www.youtube.com/watch?v={videoid}"
@@ -77,7 +46,7 @@ async def gen_thumb(videoid, user_id):
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
             try:
-                title = result["title"]
+                title = result["title"][:30]
                 title = re.sub("\W+", " ", title)
                 title = title.title()
             except:
@@ -105,14 +74,18 @@ async def gen_thumb(videoid, user_id):
 
         try:
             wxyz = await app.get_profile_photos(user_id)
-            wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{user_id}.jpg')
+            wxy = await app.download_media(
+                wxyz[0]["file_id"], file_name=f"{user_id}.jpg"
+            )
         except:
             hehe = await app.get_profile_photos(app.id)
-            wxy = await app.download_media(hehe[0]['file_id'], file_name=f'{app.id}.jpg')
+            wxy = await app.download_media(
+                hehe[0]["file_id"], file_name=f"{app.id}.jpg"
+            )
         xy = Image.open(wxy)
-        a = Image.new('L', [640, 640], 0)
+        a = Image.new("L", [640, 640], 0)
         b = ImageDraw.Draw(a)
-        b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
+        b.pieslice([(0, 0), (640, 640)], 0, 360, fill=255, outline="white")
         c = np.array(xy)
         d = np.array(a)
         e = np.dstack((c, d))
@@ -120,7 +93,7 @@ async def gen_thumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"AnonX/assets/anonx.png")
+        bg = Image.open(f"Backgrounds/{theme}.PNG")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -155,47 +128,62 @@ async def gen_thumb(videoid, user_id):
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("AnonX/assets/font2.ttf", 45)
-        ImageFont.truetype("AnonX/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("AnonX/assets/font2.ttf", 30)
-        ImageFont.truetype("AnonX/assets/font.ttf", 30)
+        font = ImageFont.truetype("assets/font2.ttf", 40)
+        ImageFont.truetype("assets/font2.ttf", 60)
+        arial = ImageFont.truetype("assets/font2.ttf", 25)
+        ImageFont.truetype("assets/font.ttf", 25)
         para = textwrap.wrap(title, width=32)
         try:
+            text_w, text_h = draw.textsize(f"ALEXA MUSIC IS PLAYING OP", font=font)
             draw.text(
-                (450, 25),
-                f"STARTED PLAYING",
-                fill="white",
-                stroke_width=3,
-                stroke_fill="grey",
+                ((1280 - text_w) / 2, 30),
+                f"ALEXA MUSIC IS PLAYING OP",
+                fill="red",
                 font=font,
+            )
+            text_w, text_h = draw.textsize(
+                f"Alexa Music One Of The Most Advanced Telegram Music Bot", font=arial
+            )
+            draw.text(
+                ((1280 - text_w) / 2, 80),
+                f"Alexa Music One Of The Most Advanced Telegram Music Bot",
+                fill="green",
+                font=arial,
             )
             if para[0]:
                 text_w, text_h = draw.textsize(f"{para[0]}", font=font)
                 draw.text(
-                    ((1280 - text_w) / 2, 530),
+                    ((1280 - text_w) / 2, 550),
                     f"{para[0]}",
-                    fill="white",
+                    fill="yellow",
                     stroke_width=1,
-                    stroke_fill="white",
+                    stroke_fill="black",
                     font=font,
                 )
             if para[1]:
                 text_w, text_h = draw.textsize(f"{para[1]}", font=font)
                 draw.text(
-                    ((1280 - text_w) / 2, 580),
+                    ((1280 - text_w) / 2, 600),
                     f"{para[1]}",
                     fill="white",
                     stroke_width=1,
-                    stroke_fill="white",
+                    stroke_fill="black",
                     font=font,
                 )
         except:
             pass
+        text_w, text_h = draw.textsize(f"YouTube: Jankari Ki Duniya", font=arial)
+        draw.text(
+            ((1280 - text_w) / 2, 620),
+            f"YouTube: Jankari Ki Duniya",
+            fill="white",
+            font=arial,
+        )
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
             ((1280 - text_w) / 2, 660),
             f"Duration: {duration} Mins",
-            fill="white",
+            fill="black",
             font=arial,
         )
         try:
@@ -206,10 +194,10 @@ async def gen_thumb(videoid, user_id):
         return f"cache/{videoid}_{user_id}.png"
     except Exception as e:
         print(e)
-        return random.choice(YOUTUBE_IMG_URL)
+        return YOUTUBE_IMG_URL
 
 
-async def gen_qthumb(videoid, user_id):
+async def gen_qthumb(videoid, user_id, theme):
     if os.path.isfile(f"cache/que{videoid}_{user_id}.png"):
         return f"cache/que{videoid}_{user_id}.png"
     url = f"https://www.youtube.com/watch?v={videoid}"
@@ -217,7 +205,7 @@ async def gen_qthumb(videoid, user_id):
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
             try:
-                title = result["title"]
+                title = result["title"][:30]
                 title = re.sub("\W+", " ", title)
                 title = title.title()
             except:
@@ -245,14 +233,18 @@ async def gen_qthumb(videoid, user_id):
 
         try:
             wxyz = await app.get_profile_photos(user_id)
-            wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{user_id}.jpg')
+            wxy = await app.download_media(
+                wxyz[0]["file_id"], file_name=f"{user_id}.jpg"
+            )
         except:
             hehe = await app.get_profile_photos(app.id)
-            wxy = await app.download_media(hehe[0]['file_id'], file_name=f'{app.id}.jpg')
+            wxy = await app.download_media(
+                hehe[0]["file_id"], file_name=f"{app.id}.jpg"
+            )
         xy = Image.open(wxy)
-        a = Image.new('L', [640, 640], 0)
+        a = Image.new("L", [640, 640], 0)
         b = ImageDraw.Draw(a)
-        b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
+        b.pieslice([(0, 0), (640, 640)], 0, 360, fill=255, outline="white")
         c = np.array(xy)
         d = np.array(a)
         e = np.dstack((c, d))
@@ -260,7 +252,7 @@ async def gen_qthumb(videoid, user_id):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"AnonX/assets/anonx.png")
+        bg = Image.open(f"Backgrounds/{theme}.PNG")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
@@ -295,47 +287,62 @@ async def gen_qthumb(videoid, user_id):
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("AnonX/assets/font2.ttf", 45)
-        ImageFont.truetype("AnonX/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("AnonX/assets/font2.ttf", 30)
-        ImageFont.truetype("AnonX/assets/font.ttf", 30)
+        font = ImageFont.truetype("assets/font2.ttf", 40)
+        ImageFont.truetype("assets/font2.ttf", 60)
+        arial = ImageFont.truetype("assets/font2.ttf", 25)
+        ImageFont.truetype("assets/font.ttf", 25)
         para = textwrap.wrap(title, width=32)
         try:
+            text_w, text_h = draw.textsize(f"ALEXA ADDED THIS SONG TO QUEUE", font=font)
             draw.text(
-                (455, 25),
-                "ADDED TO QUEUE",
-                fill="white",
-                stroke_width=5,
-                stroke_fill="black",
+                ((1280 - text_w) / 2, 30),
+                f"ALEXA ADDED THIS SONG TO QUEUE",
+                fill="red",
                 font=font,
+            )
+            text_w, text_h = draw.textsize(
+                f"Alexa Music One Of The Most Advanced Telegram Music Bot", font=arial
+            )
+            draw.text(
+                ((1280 - text_w) / 2, 80),
+                f"Alexa Music One Of The Most Advanced Telegram Music Bot",
+                fill="green",
+                font=arial,
             )
             if para[0]:
                 text_w, text_h = draw.textsize(f"{para[0]}", font=font)
                 draw.text(
-                    ((1280 - text_w) / 2, 530),
+                    ((1280 - text_w) / 2, 550),
                     f"{para[0]}",
-                    fill="white",
+                    fill="yellow",
                     stroke_width=1,
-                    stroke_fill="white",
+                    stroke_fill="black",
                     font=font,
                 )
             if para[1]:
                 text_w, text_h = draw.textsize(f"{para[1]}", font=font)
                 draw.text(
-                    ((1280 - text_w) / 2, 580),
+                    ((1280 - text_w) / 2, 600),
                     f"{para[1]}",
                     fill="white",
                     stroke_width=1,
-                    stroke_fill="white",
+                    stroke_fill="black",
                     font=font,
                 )
         except:
             pass
+        text_w, text_h = draw.textsize(f"YouTube: Jankari Ki Duniya", font=arial)
+        draw.text(
+            ((1280 - text_w) / 2, 620),
+            f"YouTube: Jankari Ki Duniya",
+            fill="white",
+            font=arial,
+        )
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
             ((1280 - text_w) / 2, 660),
             f"Duration: {duration} Mins",
-            fill="white",
+            fill="black",
             font=arial,
         )
 
@@ -348,4 +355,7 @@ async def gen_qthumb(videoid, user_id):
         return f"cache/que{videoid}_{user_id}.png"
     except Exception as e:
         print(e)
-        return random.choice(YOUTUBE_IMG_URL)
+        return YOUTUBE_IMG_URL
+
+
+
